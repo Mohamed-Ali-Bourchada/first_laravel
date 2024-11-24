0984cd40f2c76166d2fb2248e8eee3ef85d6196f
mohamed <mohamedbourchada123@gmail.com>
2024-11-24 19:13:48 +0100

    test

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -24,7 +24,7 @@
         <!-- Main Navigation -->
         <nav class="navbar navbar-expand-lg navbar-dark">
             <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link" href="{{ url('home') }}">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ url('home') }}">HomeHome</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ url('about') }}">About</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ url('service') }}">Services</a></li>
                 <li class="nav-item"><a class="nav-link" href="{{ url('contact') }}">Contact</a></li>
